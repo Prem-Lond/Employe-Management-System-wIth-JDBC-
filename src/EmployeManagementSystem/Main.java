a186bd7f9967b7e56d03b786f163f0cf50933e5e
Prem-Lond <premlond514@gmail.com>
2023-10-15 15:06:07 +0530

    remove comments

--- src/EmployeManagementSystem/Main.java
+++ src/EmployeManagementSystem/Main.java
@@ -6,20 +6,20 @@ public class Main {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         EmployeManagement employeManagement = new EmployeManagement();
-        // employeManagement.addManagersInDatabase(new Manager(111,"Shet",1000000.0));
+         employeManagement.addManagersInDatabase(new Manager(111,"Shet",1000000.0));
 
         employeManagement.showDeveloperDatabase();
         employeManagement.showManagerInDatabase();
 
 
-       /* employeManagement.addDevelopers(new Developer(1, "user1", 10000.0));
+        employeManagement.addDevelopers(new Developer(1, "user1", 10000.0));
         employeManagement.addDevelopers(new Developer(2, "user2", 20000.0));
         employeManagement.addDevelopers(new Developer(3, "user3", 30000.0));
 
-        employeManagement.addManagers(new Manager(1, "Boss", 30000.0));*/
+        employeManagement.addManagers(new Manager(1, "Boss", 30000.0));
 
 
-       /* List<Developer> developers = employeManagement.getDevelopers();
+        List<Developer> developers = employeManagement.getDevelopers();
         System.out.println("Developer List ->");
         for (Developer developer : developers) {
             System.out.println("Id     :" + developer.getId());
@@ -35,21 +35,21 @@ public class Main {
             System.out.println("Name   :" + manager.getName());
             System.out.println("Salary :" + manager.getSalary());
             System.out.println("___________________________________");
-        }*/
+        }
 
 
-        // employeManagement.addDevelopersInDatabase(new Developer(121,"premz",90000.0));
-        //  employeManagement.addDevelopersInDatabase(new Developer(122, "sakshiz", 80000.0));
-        // employeManagement.removeDevelpersInDatabase(122);
+         employeManagement.addDevelopersInDatabase(new Developer(121,"premz",90000.0));
+          employeManagement.addDevelopersInDatabase(new Developer(122, "sakshiz", 80000.0));
+         employeManagement.removeDevelpersInDatabase(122);
 
-        // employeManagement.addManagersInDatabase(new Manager(111, "Boss", 9000000.0));
-        //employeManagement.removeManagersInDatabase(111);
-        //employeManagement.showDeveloperDatabase();
+         employeManagement.addManagersInDatabase(new Manager(111, "Boss", 9000000.0));
+        employeManagement.removeManagersInDatabase(111);
+        employeManagement.showDeveloperDatabase();
 
-        //employeManagement.updateNameInDatabaseDeveloper(121,"prem");
+        employeManagement.updateNameInDatabaseDeveloper(121,"prem");
 
-        /*employeManagement.updateNameInDatabaseDeveloper(122,"sakshiz");
-        employeManagement.updateSalaryInDatabaseDeveloper(122,1000.0);*/
+        employeManagement.updateNameInDatabaseDeveloper(122,"sakshiz");
+        employeManagement.updateSalaryInDatabaseDeveloper(122,1000.0);
 
     }
 }
